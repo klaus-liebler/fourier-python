2f190d4f22d01d65816df10fbb7b8c4a4f4ca030
Klaus Liebler <mail@klaus-liebler.de>
2021-12-18 11:19:46 +0100

    Create square-fourier-series.py

---
+++ square-fourier-series.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import square
+from scipy.integrate import quad
+from math import* 
+#HalloS
+x=np.arange(-np.pi,np.pi,0.001) 
+y=square(x) 
+fc=lambda x:square(x)*cos(i*x)  
+fs=lambda x:square(x)*sin(i*x)
+n=100
+An=[] 
+Bn=[]
+sum=0
+
+for i in range(n):
+    an=quad(fc,-np.pi,np.pi)[0]*(1.0/np.pi)
+    An.append(an)
+
+for i in range(n):
+    bn=quad(fs,-np.pi,np.pi)[0]*(1.0/np.pi)
+    Bn.append(bn) 
+
+for i in range(n):
+    if i==0.0:
+        sum=sum+An[i]/2
+    else:
+        sum=sum+(An[i]*np.cos(i*x)+Bn[i]*np.sin(i*x))
+
+plt.plot(x,sum,'g')
+plt.plot(x,y,'r--')
+plt.title("Fourier-Reihe für Rechteck")
+plt.show()
